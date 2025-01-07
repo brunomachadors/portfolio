2a4b6efad341a6cb49ec67bab22cfe8a5e99a9b2
brunomachadors <brunomachadors@gmail.com>
2025-01-07 17:07:14 +0000

    Thumbnail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,30 @@ export const metadata: Metadata = {
   title: 'Bruno Machado | QA Engineer Portfolio',
   description:
     'Explore the professional portfolio of Bruno Machado, a QA Engineer with extensive experience in test automation, accessibility, and Shift Left practices.',
+  openGraph: {
+    title: 'Bruno Machado | QA Engineer Portfolio',
+    description:
+      'Explore the professional portfolio of Bruno Machado, a QA Engineer with extensive experience in test automation, accessibility, and Shift Left practices.',
+    url: 'https://bruno-portfolio-eight.vercel.app/',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dtglidvcw/image/upload/v1736269422/Portifolio/preview.png',
+        width: 1280,
+        height: 720,
+        alt: 'Bruno Machado | QA Engineer Portfolio',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Bruno Machado | QA Engineer Portfolio',
+    description:
+      'Explore the professional portfolio of Bruno Machado, a QA Engineer with extensive experience in test automation, accessibility, and Shift Left practices.',
+    images: [
+      'https://res.cloudinary.com/dtglidvcw/image/upload/v1736269422/Portifolio/preview.png',
+    ],
+  },
 };
 
 export default function RootLayout({
