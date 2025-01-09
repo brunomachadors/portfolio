38cf9495526a4b54968aa3b646c0f07d6ff52199
brunomachadors <brunomachadors@gmail.com>
2025-01-09 11:07:56 +0000

    update bout section

---
+++ src/app/content/about.tsx
@@ -0,0 +1,74 @@
+export const sections = [
+  {
+    title: 'Personal Information',
+    content: (
+      <div>
+        <p>
+          <strong>Location:</strong> Porto, Portugal
+        </p>
+        <p>
+          <strong>Nationality:</strong> Brazilian
+        </p>
+        <p>
+          <strong>Year of Birth:</strong> 1986
+        </p>
+      </div>
+    ),
+  },
+
+  {
+    title: 'My Journey into QA',
+    content:
+      'I started working in QA by chance and quickly realized that I have a strong aptitude for it. With over 17 years of experience in testing, I have specialized in test automation since 2017.',
+  },
+  {
+    title: 'Focus on Shift Left Practices',
+    content:
+      'I actively promote early involvement of QA in the development process, collaborating with developers to implement test frameworks and identify potential issues before they arise.',
+  },
+  {
+    title: 'Collaboration and Mentoring',
+    content:
+      'I value building strong relationships with colleagues, fostering a collaborative work environment, and mentoring professionals in the testing field.',
+  },
+  {
+    title: 'Educational Background - Technology',
+    content: (
+      <div className="flex flex-col gap-4">
+        <p className="pt-2">
+          <strong>University:</strong> União Educacional de Brasília (UNEB)
+        </p>
+        <p className="pt-2">
+          <strong>Course:</strong> Technologist in Information Technology
+        </p>
+        <p className="pt-2">
+          <strong>Period:</strong> 2008 - 2010
+        </p>
+        <p className="pt-2">
+          <strong>Description:</strong> Emphasis on Data Processing and
+          Programming.
+        </p>
+      </div>
+    ),
+  },
+  {
+    title: 'Educational Background - Game Development',
+    content: (
+      <div className="flex flex-col gap-4">
+        <p className="pt-2">
+          <strong>University:</strong> Pontifícia Universidade Católica do Rio
+          Grande do Sul (PUCRS)
+        </p>
+        <p className="pt-2">
+          <strong>Course:</strong> Postgraduate in Game Development
+        </p>
+        <p className="pt-2">
+          <strong>Period:</strong> 2011 - 2012
+        </p>
+        <p className="pt-2">
+          Focused on Game Design, Programming, and 3D Modeling.
+        </p>
+      </div>
+    ),
+  },
+];
