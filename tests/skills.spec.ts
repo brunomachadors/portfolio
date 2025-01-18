8f39436eeea338e8826bf8b6d715a947e2712c13
brunomachadors <brunomachadors@gmail.com>
2025-01-18 08:16:20 +0000

    Update test step text

--- tests/skills.spec.ts
+++ tests/skills.spec.ts
@@ -2,7 +2,7 @@ import test from 'playwright/test';
 import { SkillsPage } from './pages/skillsPage';
 import { SKILLS } from '@/app/content/skills';
 
-test.describe('Skills Page', () => {
+test.describe('Skills', () => {
   test('Content', async ({ page }) => {
     const skillsPage = new SkillsPage(page);
 
@@ -16,23 +16,28 @@ test.describe('Skills Page', () => {
         await skillsPage.validateCategoryTab(category);
       }
     });
-
-    for (const { category, subcategories } of SKILLS) {
-      await test.step(`Category: ${category}`, async () => {
-        await skillsPage.validateCategoryTab(category);
-
-        for (const { name, items } of subcategories) {
-          await test.step(`Subcategory: ${name}`, async () => {
-            await skillsPage.validateSubCategory(name);
+    await test.step(`Category`, async () => {
+      for (const { category, subcategories } of SKILLS) {
+        await test.step(`${category}`, async () => {
+          await test.step(`Visible`, async () => {
+            await skillsPage.validateCategoryTab(category);
           });
-
-          for (const { text, description } of items) {
-            await test.step(`Skill: ${text}`, async () => {
-              await skillsPage.validateSkill(text, description);
-            });
-          }
-        }
-      });
-    }
+          await test.step(`Subcategories`, async () => {
+            for (const { name, items } of subcategories) {
+              await test.step(`${name}`, async () => {
+                await skillsPage.validateSubCategory(name);
+              });
+              await test.step(`Skills`, async () => {
+                for (const { text, description } of items) {
+                  await test.step(` ${text}`, async () => {
+                    await skillsPage.validateSkill(text, description);
+                  });
+                }
+              });
+            }
+          });
+        });
+      }
+    });
   });
 });
